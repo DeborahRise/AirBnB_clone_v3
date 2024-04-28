69c7df0a5e7f99cf2fe658a78898c3bf83e9f47e
emarvelm <marvelousegbe19@gmail.com>
2024-04-28 15:32:30 +0100

    remove all importation of class since not used

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,13 +5,6 @@ This module contains endpoint(route) status
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
