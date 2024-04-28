cac951f07fb84a4a6153e8f6b90b2105e406539c
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-28 18:04:32 +0100

    lol. silly mistake. it shoulb be @app_views.route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,30 +2,26 @@
 
 """  a new view for State objects that handles all default RESTFul API actions """
 """  use to_dict() to retrieve an object into a valid JSON """
+from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.state import State
-from api.v1.views import app_views
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
+    
 
-
-app.Flask = (__name__)
-
-app.route(/api/v1/states, strict_slashes = False)
+@app_views.route(/api/v1/states, strict_slashes = False)    
 def get_states():
     all_states = []
     return jsonify(all_states.append([state.to_dict() for state in storage.all(State).values()]))
 
-app.route(/api/v1/states/<state_id>, strict_slashes = False)
+@app.route(/api/v1/states/<state_id>, strict_slashes = False)
 def get_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
 
-app.route(/api/v1/states/<state_id>, methods=['DELETE'], strict_slashes = False)
+@app.route(/api/v1/states/<state_id>, methods=['DELETE'], strict_slashes = False)
 def delete_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
@@ -34,7 +30,7 @@ def delete_state(state_id):
     storage.save()
     return jsonify({}), 200
 
-app.route(/api/v1/states, methods=['POST'], strict_slashes = False)
+@app.route(/api/v1/states, methods=['POST'], strict_slashes = False)
 def post_state():
     if not request.json:
         abort(400, "Not a JSON")
@@ -45,7 +41,7 @@ def post_state():
     storage.save()
     return jsonify(state.to_dict()), 201
 
-app.route(/api/v1/states/<state_id>, methods=['PUT'], strict_slashes = False)
+@app.route(/api/v1/states/<state_id>, methods=['PUT'], strict_slashes = False)
 def put_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
