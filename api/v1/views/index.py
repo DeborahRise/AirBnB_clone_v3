f14e5cc5e434c96f296b1c87941a2ada4a15fb85
emarvelm <marvelousegbe19@gmail.com>
2024-04-28 15:30:31 +0100

    expecting a string not a class

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -25,14 +25,14 @@ def status():
     return jsonify(status="OK")
 
 
-@app_views.route('/api/v1/stats', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats():
     _count ={
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
     }
     return jsonify(_count)
