d21c99da35b9bd7bfccb0f6ef53c256869a25ee5
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-29 14:32:34 +0100

    create_city method created

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -2,13 +2,14 @@
 """City objects that handles all default RESTFul API actions"""
 
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort,request
 from models.state import State
 from models.city import City
 from models import storage
 
 @app_views.route("/states/<state_id>/cities", strict_slashes=False)
 def get_cities(state_id):
+    """ Retrieves the list of all City objects of a State """
     cities_list = []
 
     state_obj = storage.get(State, state_id)
@@ -22,8 +23,35 @@ def get_cities(state_id):
 
 @app_views.route("/cities/<city_id>", strict_slashes=False, methods=["GET"])
 def get_city(city_id):
+    """ Retrieves a City object """
     city_obj = storage.get(City, city_id)
     if not city_obj:
         abort(404)
-    city_obj.__class__
     return jsonify(city_obj.to_dict())
+
+@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["DELETE"])
+def delete_city(city_id):
+    """ Deletes a City object: DELETE /api/v1/cities/<city_id> """
+    city_obj = storage.get(City, city_id)
+    if not city_obj:
+        abort(404)
+    storage.delete(city_obj)
+    storage.save()
+    return ({}, 200)
+
+@app_views.route("/states/<state_id>/cities", strict_slashes=False, methods=["POST"])
+def create_city(state_id, ):
+    """ Creates a City: POST /api/v1/states/<state_id>/cities """
+    state_obj = storage.get(State, state_id)
+    if not state_obj:
+        abort(404)
+    
+    city_data = request.get_json(force=True, silent=True)
+    if not city_data:
+        abort(404, "Not a JSON")
+    if 'name' not  in city_data:
+        abort(404, "Missing name")
+    city_data["state_id"] = state_id
+    city_obj = City(**city_data)
+    city_obj.save()
+    return jsonify(city_obj.to_dict()), 201
