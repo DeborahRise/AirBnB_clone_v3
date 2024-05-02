62c72077be346c9b28c6ab7e4e0aca25a605eb73
DeborahRise <debie.rise.bookings@gmail.com>
2024-05-02 21:27:51 +0100

    testing new token and pushing plaaces_amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -2,3 +2,13 @@
 """  a new view for the link between Place objects and Amenity objects
 that handles all default RESTFul API actions"""
 
+from api.v1.views import app_views
+from models import storage
+from models.place import Place
+from models.amenity import Amenity
+from flask import jsonify, abort
+
+@app_views.route("places/<place_id>/amenities", strict_slashes=False, methods=["GET"])
+def get_amenitiesOfPlace(place_id):
+    place_obj = storage.get(Place, place_id)
+    
