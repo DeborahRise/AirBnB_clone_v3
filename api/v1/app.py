9ffc747408f74951ede585bab7b1c6f3d7cc6542
EMarvelM <marvelousegbe19@gmail.com>
2024-05-02 18:39:07 +0100

    configured CORS to allow cross-origin requests from 0.0.0.0 for all routes /*

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,11 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r'/*': {'origins': 'http://0.0.0.0'}})
 
 
 @app.teardown_appcontext
